dace28a24cf3f0135871bdcd1f06c5ebef816f35
黑弩 <118418714+heinu123@users.noreply.github.com>
2023-03-08 15:38:06 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import click
 from main import main
 
 @click.command()
-@click.option('--port', prompt='你的节点端口(多个端口请使用,分割) ', required=True, default="", help='节点端口', type=(str))
+@click.option('--port', prompt='你的节点端口(多个端口请使用,分割) ', required=True, help='节点端口', type=(str))
 @click.option('--web_port', prompt='你的白名单网页认证端口 默认:', required=True, default="5563", help='白名单网页认证端口', type=(str))
 @click.option('--url_path', prompt='你的白名单网页认证路径 默认:', required=True, default="/", help='白名单网页认证路径', type=(str))
 @click.option('--username', prompt='你的白名单网页认证账号 默认:', required=True, default="123", help='白名单网页认证账号', type=(str))
